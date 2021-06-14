7e4483f146c6fea7530420f53a59581bd3f65598
Sandesh <pdlsandesh144@gmail.com>
2021-06-14 18:04:25 +0545

    broken our homepages into smaller components and style it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage.component";
 import './App.css';
 
 
---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory from "../Components/directory/directory.component";
+import './homepage.styles.scss'
+
+const HomePage = () =>(
+    <div className='homepage'>
+    <Directory />
+    </div>
+);
+
+export default HomePage;
