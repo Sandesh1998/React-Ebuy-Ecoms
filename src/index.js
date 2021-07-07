ef6914c95931623d484d70b4068309f3a2d2293a
Sandesh <pdlsandesh144@gmail.com>
2021-07-07 13:20:34 +0545

    implementing the localstorage to store the cart data using redux-persist

--- src/index.js
+++ src/index.js
@@ -2,19 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 
 import './index.css';
 import App from './App';
- import store from './redux/store'
+import { store, persistor } from './redux/store'
 
 
 ReactDOM.render(
   <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </Provider>
-  ,
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
